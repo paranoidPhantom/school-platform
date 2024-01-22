7f2c26c10ee919c7c1db868dfa37fe8f6c554cac
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-01-22 17:36:39 +0300

    Working on HW list

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,52 @@
 </script>
 
 <template>
-    <main>
+    <main class="__home-page">
         <PrimaryTable />
+		<UCard class="card" v-if="false">
+			<div class="content">
+				<div class="filters">
+					<h4>Фильтры</h4>
+				</div>
+				<div class="divider"></div>
+				<div class="list">
+					
+				</div>
+			</div>
+		</UCard>
     </main>
 </template>
 
 <style scoped lang="scss">
 
+.card {
+	max-width: min(1000px, 95%);
+	margin: 2rem auto;
+	.content {
+		display: flex;
+		align-items: center;
+		.filters {
+			padding: 1rem;
+			width: 100%;
+			flex-shrink: 3.5;
+		}
+		.divider {
+			border-right: 1px solid rgba(255, 255, 255, 0.1);
+			height: 100%;
+        }
+		.list {
+			width: 100%;
+			padding: 1rem;
+		}
+	}
+}
+
+@media (max-width: 600px) {
+	.content {
+		flex-wrap: wrap;
+		.divider {
+			display: none;
+		}
+	}
+}
 </style>
