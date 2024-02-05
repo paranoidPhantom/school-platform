1472cda3207a0ac4ccaca9e937d7c843a6221314
Andrei Hudalla <andrei.hudalla@outlook.com>
2024-02-05 22:25:53 +0300

    Added realtime to homework

--- app.vue
+++ app.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-const supabase = useSupabaseClient();
+import type { RealtimePostgresChangesPayload } from "@supabase/supabase-js";
 
+const supabase = useSupabaseClient();
 
 const homework = useState("homework_global", () => {
     return {};
@@ -10,10 +11,17 @@ const homework_arr = useState("homework_global_array", () => {
     return [];
 });
 
+
 const { query } = useRoute();
 
 const route = useRoute();
 const router = useRouter();
+
+defineShortcuts({
+	shift_a: () => {
+		router.push("/homework/moderate")
+	}
+})
 
 const modal = useState<{
     open: boolean;
@@ -34,12 +42,12 @@ const modal = useState<{
 });
 
 watchEffect(() => {
-	useHead({
-		titleTemplate: (title) =>
-			title ? `${title} | 10Б` : `Домашнее задание | 10Б`,
-		title: route.meta.title ? route.meta.title : undefined
-	});
-})
+    useHead({
+        titleTemplate: (title) =>
+            title ? `${title} | 10Б` : `Домашнее задание | 10Б`,
+        title: route.meta.title ? route.meta.title : undefined,
+    });
+});
 
 router.afterEach(() => (modal.value.open = false));
 
@@ -50,54 +58,107 @@ onMounted(() => {
     }
 });
 
-const loading = ref(true)
+const loading = ref(true);
 
-onMounted(async () => {
-    homework.value = {};
-    const { data } = await supabase.from("homework").select("*, comments(*)");
-	homework_arr.value = data
-    data?.forEach((hw) => {
+const recalculateHW = () => {
+    homework_arr.value.forEach((hw) => {
         const subj = hw.subject;
         if (!homework.value[subj]) homework.value[subj] = [];
         homework.value[subj].push(hw);
     });
+}
+
+watch(homework_arr, recalculateHW)
+
+onMounted(async () => {
+    const wait = setInterval(() => {
+        if (loading.value === false) clearInterval(wait);
+        loading.value = false;
+    }, 100);
+
+    homework.value = {};
+    const { data } = await supabase.from("homework").select("*, comments(*)");
+    homework_arr.value = data;
 });
 
-onMounted(() => {
-	const wait = setInterval(() => {
-		if (loading.value === false) clearInterval(wait)
-		loading.value = false
-	}, 100)
-})
+
+const handle_postgres_changes = (
+    payload: RealtimePostgresChangesPayload<{ [key: string]: any }>
+) => {
+    const hw = payload.eventType !== "DELETE" ? payload.new : payload.old;
+    switch (payload.eventType) {
+        case "INSERT":
+			homework_arr.value.push(hw)
+            break;
+        case "UPDATE":
+            homework_arr.value.forEach(
+                (hw_checking, index: number) => {
+                    if (hw_checking.id === hw.id) {
+						homework_arr.value[index] = hw
+                    }
+                }
+            );
+            break;
+        case "DELETE":
+            homework_arr.value.forEach(
+                (hw_checking, index: number) => {
+                    if (hw.id === hw_checking.id) {
+						homework_arr.value.splice(index, 1)
+                    }
+                }
+            );
+            break;
+    }
+};
+
+const listener = supabase
+    .channel("custom-all-channel")
+    .on(
+        "postgres_changes",
+        { event: "*", schema: "public", table: "homework" },
+        handle_postgres_changes
+    )
+    .subscribe();
 </script>
 
 <template>
-	<div id="app_container">
-		<ProfileSetup />
-		<Transition name="loader">
-			<div v-if="loading" class="loader">
-				<Icon style="font-size: 4rem; opacity: 0.5;" name="svg-spinners:ring-resize"/> 
-				<p style="font-size: 1.5rem; opacity: 0.8;">Доброе утро!</p>
-			</div>
-		</Transition>
-		<Head>
-			<Link
-				rel="apple-touch-icon"
-				href="/icons/apple_homework.png"
-				sizes="180x180"
-			/>
-		</Head>
-		<GlobalHeader />
-		<NuxtPwaManifest />
+    <div id="app_container">
+        <NuxtLoadingIndicator color="rgb(var(--color-primary-DEFAULT))" />
+        <ProfileSetup />
+        <Transition name="loader">
+            <div v-if="loading" class="loader">
+                <Icon
+                    style="font-size: 4rem; opacity: 0.5"
+                    name="svg-spinners:ring-resize"
+                />
+                <p style="font-size: 1.5rem; opacity: 0.8">Доброе утро!</p>
+            </div>
+        </Transition>
+        <Head>
+            <Link
+                rel="apple-touch-icon"
+                href="/icons/apple_homework.png"
+                sizes="180x180"
+            />
+        </Head>
+        <GlobalHeader />
+        <NuxtPwaManifest />
         <ModalWindow />
         <NuxtPage />
-		<UNotifications />
+        <UNotifications />
     </div>
 </template>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&display=swap");
+
+html, body {
+    max-width: 100vw;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+}
 
 body {
     margin: 0;
@@ -111,25 +172,25 @@ body {
 }
 
 .app_container {
-	overflow-x: hidden;
+    overflow-x: hidden;
 }
 
 .object-cover {
-	* {
-		object-fit: cover;
-	}
+    * {
+        object-fit: cover;
+    }
 }
 
 .loader {
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	gap: 2rem;
-	position: fixed;
-	z-index: 103;
-	inset: 0;
-	background-color: rgb(10, 10, 10);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    position: fixed;
+    z-index: 103;
+    inset: 0;
+    background-color: rgb(10, 10, 10);
 }
 
 h1,
@@ -156,21 +217,20 @@ span {
     z-index: 2;
 }
 
-
 .loader-enter-from,
 .loader-leave-to {
     opacity: 0;
-	> p {
-		opacity: 0 !important;
-	}
+    > p {
+        opacity: 0 !important;
+    }
 }
 
 .loader-enter-active,
 .loader-leave-active {
     transition: all 0.5s ease-in-out 0.2s;
-	p {
-		transition: all 0.5s;
-	}
+    p {
+        transition: all 0.5s;
+    }
 }
 
 .page-enter-from,
